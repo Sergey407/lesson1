de10127edc95f71a5c37d1569ef9aa860fbc99fb
Sergey407 <karawaschencko@yandex.com>
2024-10-09 19:46:33 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+kolichestvo_vipolnennih_dz=12
+kolichestvo_zatrachennix_chasov=1.5
+nazvanie_kursa='Python'
+vremya_na_odno_zadanie=kolichestvo_zatrachennix_chasov/kolichestvo_vipolnennih_dz
+print('nazvanie_kursa', ": ", nazvanie_kursa, ', ',"kolichestvo_zatrachennix_chasov", ": ",kolichestvo_zatrachennix_chasov, ', ',"kolichestvo_vipolnennih_dz", ": ",kolichestvo_vipolnennih_dz,', ', "vremya_na_odno_zadanie",": ",vremya_na_odno_zadanie," часа")
+
+
+
